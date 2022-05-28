c965af740032bb1fb424fcac579b120c5519c1c1
jairacagomoc <jhairabcagomoc@gmail.com>
2022-05-28 10:10:08 +0800

    first commit

---
+++ 1.10CodingExercise/main.py
@@ -0,0 +1,9 @@
+character_job = "pirate"
+character_ride = "ship"
+character_souvenir = "gold"
+character_pet = "parrot"
+
+print("There was once a " + character_job + "who loved adventure.")
+print("The" + character_job + "would take her." + character_ride + "to unknown places.")
+print("She brings hose a lot of" + character_souvenir + ".")
+print("Then she goes hose to her " + character_pet + ", Chuckles.")

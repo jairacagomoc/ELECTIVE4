8ce19d1641c8b402d15e48bef65c9aa76c9ae1f6
jairacagomoc <jhairabcagomoc@gmail.com>
2022-05-28 16:49:08 +0800

    2nd fifth exercise

---
+++ pythonProject2/2nd-3.4CodingExercise/main.py
@@ -0,0 +1,7 @@
+f = open ("pythoneyssay.txt", "w")
+f.write("I like Python because it's very interesting.")
+f = open("pythonessay.txt", "a")
+f.write("\nI plan to learn data vusualization")
+f.write("\nI plan to make a better world for the future generation")
+f = open("pythonessay.txt", "r")
+f.close()

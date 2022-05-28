fac3630b97a814fe72830dcc2d20c04f84cc9446
jairacagomoc <jhairabcagomoc@gmail.com>
2022-05-28 10:15:53 +0800

    sixth exercise

---
+++ 3.4CodingExercises/main.py
@@ -0,0 +1,18 @@
+class Customers:
+    greeting = "Welcome to Coffee Palace!"
+
+c_1 = Customers()
+c_1.name = "Samirah"
+c_1.beverage = "Iced caffe latte"
+c_1.food = "Cinnamon roll"
+c_1.total = 225
+
+c_2 = Customers()
+c_2.name = "Samirah"
+c_2.beverage = "Iced caffe latte"
+c_2.food = "Cinnamon roll"
+c_2.total = 225
+
+print(Customers.greeting)
+print(c_1.beverage)
+print(c_2.food)
